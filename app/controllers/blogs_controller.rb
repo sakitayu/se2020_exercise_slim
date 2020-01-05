46b7a0980b525c28df0ad68a1a97d33443f4728b
sakitayu <sakitakitakita@gmail.com>
2020-01-05 12:25:26 +0900

    indexページとshowページを作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,14 +1,29 @@
 class BlogsController < ApplicationController
+  
   def index
+    @blogs = Blog.all
   end
 
   def new
+    @blog = Blog.new
+  end
+
+  def create
+    Blog.create(blog_params)
+    redirect_to blogs_path
   end
   
 
   def show
+    @blog = Blog.find(params[:id])
   end
 
   def edit
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :content)
   end
 end
